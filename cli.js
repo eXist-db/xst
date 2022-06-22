4aca412f0d39b084d62c3447a2592f883b596417
Juri Leino <github@line-o.de>
2022-06-22 14:07:31 +0200

    feat(help): add logo, examples to default output

--- cli.js
+++ cli.js
@@ -6,21 +6,61 @@ import { handleError } from './utility/errors.js'
 import { hideBin } from 'yargs/helpers'
 import { readConnection } from './utility/connection.js'
 
+/**
+ * if package was linked to global strip relative path from output
+ * @param {String} $0 raw value of argv.$0
+ * @returns {String} script name with relative path stripped if linked
+ */
+function getScriptName($0) {
+  if ($0.startsWith('.')) {
+    return $0.substring($0.lastIndexOf('/') + 1)
+  }
+  return $0
+}
+
+function showCompletionHelp(scriptName) {
+  console.log(`
+You can install completions for ZSH and BASH. For details have a look at the
+comments in the output of
+
+  ${scriptName} completion`)
+}
+
+function showExamples(scriptName) {
+  console.log(`
+Examples:
+  ${scriptName} ls --extended /db/apps
+  ${scriptName} install ./my-package.xar
+  ${scriptName} run 'count(//p)'
+`)
+}
+
+function showLogo() {
+  console.log(`
+ ╲ ╱  ╓───   ──┰──
+  ╳   ╰───╮    │ 
+ ╱ ╲  ▂▁▁▁│    ┇
+`)
+}
+
 const parser = yargs(hideBin(process.argv))
-  .scriptName('xst')
   .middleware(readConnection)
   .command('$0', 'interact with an exist-db instance', () => {}, (argv) => {
-    commands.map(a => console.log(a.command, a.describe))
+    showLogo()
+    parser.showHelp()
+    const scriptName = getScriptName(argv.$0)
+    showCompletionHelp(scriptName)
+    // append examples
+    showExamples(scriptName)
   })
   .help()
-  // .completion('completion', function (current, argv, completionFilter, done) {
-  //   completionFilter()
-  // })
+  .completion('completion', false)
   .command(commands)
   .demandCommand(1)
   .strict(true)
-  .wrap(96)
   .fail(false)
+
+parser.wrap(parser.terminalWidth())
 
 async function run () {
   return await parser.parse()
