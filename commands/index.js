de23b1ee0dc3f8d6084d64e99e18e909931692be
Juri Leino <github@line-o.de>
2022-07-04 23:04:06 +0200

    chore(tree): remove tree command
    BREAKING CHANGE: the tree command is obsolete.
    Use xst list --tree instead.

--- commands/index.js
+++ commands/index.js
@@ -1,13 +1,11 @@
 import * as exec from './exec.js'
 import * as install from './install.js'
 import * as list from './list.js'
-import * as tree from './tree.js'
 import * as upload from './upload.js'
 
 export const commands = [
   exec,
   install,
   list,
-  tree,
   upload
 ]
