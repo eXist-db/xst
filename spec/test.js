87b93cdc98702b4e27df70bc955ee3563b3ff5b9
Juri Leino <github@line-o.de>
2022-12-11 18:33:08 +0100

    feat(test): add facility to test pipes

--- spec/test.js
+++ spec/test.js
@@ -18,4 +18,25 @@ export async function run (cmd, args, options) {
   })
 }
 
+export async function runPipe (cmd1, args1, cmd2, args2, options) {
+  return new Promise((resolve, reject) => {
+    let stderr
+    let stdout
+    const proc1 = spawn(cmd1, args1, options)
+    const proc2 = spawn(cmd2, args2, options)
+    proc1.stdout.pipe(proc2.stdin)
+
+    proc2.stdout.on('data', data => {
+      stdout ? stdout += data.toString() : stdout = data.toString()
+    })
+
+    proc2.stderr.on('data', data => {
+      stderr ? stderr += data.toString() : stderr = data.toString()
+    })
+    proc2.on('close', _ => resolve({ stderr, stdout }))
+    proc1.on('error', error => reject(error))
+    proc2.on('error', error => reject(error))
+  })
+}
+
 export const asAdmin = { env: { ...process.env, EXISTDB_USER: 'admin', EXISTDB_PASS: '' } }
