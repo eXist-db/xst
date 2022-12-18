51819c2b323c4309a9cfebd4bcd2dc860904ac34
Juri Leino <github@line-o.de>
2022-12-18 21:44:57 +0100

    feat: add new command info
    `xst info` will gather useful system information.
    The output can directly be used to add to eXist issues.

--- commands/index.js
+++ commands/index.js
@@ -1,15 +1,17 @@
 import * as exec from './exec.js'
-import * as pkg from './package/index.js'
-import * as list from './list.js'
-import * as upload from './upload.js'
-import * as rm from './rm.js'
 import * as get from './get.js'
+import * as info from './info.js'
+import * as list from './list.js'
+import * as pkg from './package/index.js'
+import * as rm from './rm.js'
+import * as upload from './upload.js'
 
 export const commands = [
   exec,
-  pkg,
+  get,
+  info,
   list,
-  upload,
+  pkg,
   rm,
-  get
+  upload
 ]
---
+++ spec/tests/info.js
@@ -0,0 +1,14 @@
+import { test } from 'tape'
+import { run, asAdmin } from '../test.js'
+
+test("calling 'xst info'", async (t) => {
+  const { stderr, stdout } = await run('xst', ['info'], asAdmin)
+  if (stderr) { return t.fail(stderr) }
+
+  const lines = stdout.split('\n')
+  t.equal(lines.length, 4, 'outputs four lines')
+  t.ok(lines[0].startsWith('Build: '), 'has build info')
+  t.ok(lines[1].startsWith('Java: '), 'has Java info')
+  t.ok(lines[2].startsWith('OS: '), 'has OS info')
+  t.end()
+})
