2e5636da1a3d5a1a6d8de644ad436a5d0d4c2f67
Juri Leino <github@line-o.de>
2023-01-10 00:19:35 +0100

    ci(commitlint): it's the rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
-  rule: { 'body-max-line-length': [1, 'always', 120] },
+  rules: {
+    // lower severity to warning to allow longer lines
+    'body-max-line-length': [1, 'always', 120]
+  },
   extends: ['@commitlint/config-conventional']
 }
