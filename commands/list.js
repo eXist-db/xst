1456eb20cd9f86781b9b8974caa1b40756404421
Juri Leino <github@line-o.de>
2023-01-10 12:46:17 +0100

    chore(list): add and use glob.js utility

--- commands/list.js
+++ commands/list.js
@@ -5,7 +5,7 @@ import { getDateFormatter } from '../utility/colored-date.js'
 import { multiSort } from '../utility/sorter.js'
 import { recursivePadReducer } from '../utility/padding.js'
 import { getSizeFormatter } from '../utility/size.js'
-
+import { getGlobMatcher } from '../utility/glob.js'
 /**
  * @typedef { import("node-exist").NodeExist } NodeExist
  */
@@ -135,31 +135,6 @@ function getTreeFormatter (options) {
 }
 
 // name
-
-/**
- * transform globbing pattern to regular expression
- * @param {String} glob globbing pattern
- * @returns {String} regular expression
- */
-function toRegExpPattern (glob) {
-  const converted = glob
-    .replace(/\\/g, '\\\\') // escape backslashes
-    .replace(/\./g, '\\.') // make . literals
-    .replace(/\?/g, '.') // transform ?
-    .replace(/\*/g, '.*?') // transform *
-
-  return `^${converted}$`
-}
-
-/**
- * get filter function that checks item names against globbing pattern
- * @param {String} glob globbing pattern
- * @returns {(item:ListResultItem) => Boolean}
- */
-function getGlobMatcher (glob) {
-  const regex = new RegExp(toRegExpPattern(glob), 'i')
-  return (item) => regex.test(item.name)
-}
 
 /**
  * show displayName of item depending on its type
