0a8d0158fa1f32efa9cbb0a11d456aa965d94b8f
Juri Leino <github@line-o.de>
2025-02-25 16:40:58 +0100

    test: adapt output assertions of no REST tests

--- spec/norest/package/install.js
+++ spec/norest/package/install.js
@@ -26,14 +26,12 @@ test('package install without REST', async function (t) {
 
     const lines = stdout.split('\n')
     st.equal(lines[0], 'Falling back to XMLRPC API')
-    st.equal(lines[1], 'Install test-lib.xar on https://localhost:8443')
-    st.equal(lines[2], '✔︎ uploaded')
-    st.equal(lines[3], '✔︎ installed')
+    st.equal(lines[1], '✔︎ spec/fixtures/test-lib.xar > installed test-lib@1.0.0')
     st.end()
   })
 
   t.test('succeeds when enforcing upload over XMLRPC', async function (st) {
-    const { stderr, stdout } = await run('xst', ['package', 'install', 'local', '--rpc', 'spec/fixtures/test-lib.xar'], asAdmin)
+    const { stderr, stdout } = await run('xst', ['package', 'install', 'local', '--rpc', 'spec/fixtures/test-lib.xar', '-f'], asAdmin)
     if (stderr) {
       st.fail(stderr)
       st.end()
@@ -42,13 +40,12 @@ test('package install without REST', async function (t) {
 
     const lines = stdout.split('\n')
     st.equal(lines[0], 'Install test-lib.xar on https://localhost:8443')
-    st.equal(lines[1], '✔︎ uploaded')
-    st.equal(lines[2], '✔︎ updated')
+    st.equal(lines[1], '✔︎ spec/fixtures/test-lib.xar > installed test-lib@1.0.0')
     st.end()
   })
 
   t.test('fails with enforced upload over REST', async function (st) {
-    const { stderr, stdout } = await run('xst', ['package', 'install', 'local', '--rest', 'spec/fixtures/test-lib.xar'], asAdmin)
+    const { stderr, stdout } = await run('xst', ['package', 'install', 'local', '--rest', 'spec/fixtures/test-lib.xar', '-f'], asAdmin)
 
     st.equal(stdout, 'Install test-lib.xar on https://localhost:8443\n')
 
