f747ff0c1f17c2f63f18f250574060add54b1894
Juri Leino <github@line-o.de>
2025-04-27 22:35:14 +0200

    test: add test for xst info --verbose

--- spec/tests/info.js
+++ spec/tests/info.js
@@ -2,11 +2,23 @@ import { test } from 'tape'
 import { run, asAdmin } from '../test.js'
 
 test("calling 'xst info'", async (t) => {
-  const { stderr, stdout } = await run('xst', ['info'], asAdmin)
+  const { stderr, stdout } = await run('xst', ['info'])
   if (stderr) { return t.fail(stderr) }
 
   const lines = stdout.split('\n')
   t.plan(4)
+  t.equal(lines.length, 4, 'outputs four lines')
+  t.ok(lines[0].startsWith('Build: '), 'has build info')
+  t.ok(lines[1].startsWith('Java: '), 'has Java info')
+  t.ok(lines[2].startsWith('OS: '), 'has OS info')
+})
+
+test("calling 'xst info --verbose'", async (t) => {
+  const { stderr, stdout } = await run('xst', ['info', '--verbose'])
+  t.plan(5)
+  t.equal(stderr, 'Connecting to https://localhost:8443 as guest\n', 'connection and user output on stderr')
+
+  const lines = stdout.split('\n')
   t.equal(lines.length, 4, 'outputs four lines')
   t.ok(lines[0].startsWith('Build: '), 'has build info')
   t.ok(lines[1].startsWith('Java: '), 'has Java info')
